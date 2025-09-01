3a9f9b5d014e8cc9bdf8f4dc7a0671475072c463
Kenneth Carter <kccarter32@gmail.com>
2025-08-31 18:09:34 -0600

    map projects for solution and create .Net Testing project

---
+++ SubLight.Net.Testing/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SubLight.Net.Testing
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
